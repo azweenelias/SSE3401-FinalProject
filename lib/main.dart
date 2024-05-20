5b5c74c6a29a4b0cf797e3a5cb32bef9a861f9d8
junhubdotcom <furnoyong@gmail.com>
2024-05-20 23:25:57 +0800

    mainlattest

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false, home: const FactoryPage());
+        debugShowCheckedModeBanner: false, home: const LoginPage());
   }
 }
 
